f27aca673ef361d4bc1cd850bc8af11808ffa25e
M <michael.j.liu@telus.com>
2015-08-13 21:33:29 -0700

    tidy up

--- R/pml.R
+++ R/pml.R
@@ -6,17 +6,17 @@
 # as the data already been saved to data/pml.RData
 # no need to download them again
 #
+library(caret)
 data(pml)
 
-library(caret)
 
 #
 # split dataset into training and validation
 # the pml_testing is for prediction and submission
 #
 trainIndex <- createDataPartition(pml_train$classe, p=.7, list=F)
-train <- pml_train[trainIndex, ]
-validate <- pml_train[-trainIndex, ]
+train      <- pml_train[trainIndex, ]
+validate   <- pml_train[-trainIndex, ]
 
 #
 # variable selection
@@ -26,7 +26,8 @@ fit <- knn(train, validate, train$classe, k=5)
 #
 # x validation aka confusion matrix
 #
-table(predict(fit, validation)$pred, validation$classe)
+prediction <- predict(fit, validation)
+table(prediction, validation)
 
 #
 # other algo to try, svm, decision tree, random forest, naive bayes classifier
